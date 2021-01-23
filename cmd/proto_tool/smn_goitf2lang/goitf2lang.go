300ca8ffd5b8d16f802e64a1910cc0d29e7d9080
protossgenius <guyvejianglou@outlook.com>
2021-01-23 14:34:16 +0800

    update some package name.

---
+++ cmd/proto_tool/smn_goitf2lang/goitf2lang.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ProtossGenius/smnric/analysis/smn_rpc_itf"
+	"github.com/ProtossGenius/smnric/proto_tool/goitf2lang"
+)
+
+func checkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	i := flag.String("i", "./rpc_itf/", "rpc interface dir.")
+	o := flag.String("o", "./cpp_itf/", "rpc needs proto output.")
+	lang := flag.String("lang", "cpp", "lang to translate, now support [cpp]")
+	flag.Parse()
+	err := os.MkdirAll(*o, os.ModePerm)
+	checkerr(err)
+	itfs, err := smn_rpc_itf.GetItfListFromDir(*i)
+	checkerr(err)
+	for _, list := range itfs {
+		pkg := list[0].Package
+		goitf2lang.WriteInterface(*lang, *o, pkg, list)
+	}
+}
